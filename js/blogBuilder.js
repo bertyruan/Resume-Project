dbfb3b96454227db903156a830b733fe2f56310b
chinesebaloni <berty.ruan@gmail.com>
2015-06-22 20:52:32 -0400

    Create my favicon with javascript/html5

--- js/blogBuilder.js
+++ js/blogBuilder.js
@@ -3,3 +3,31 @@ author: Berty Ruan
 file: js/blogIndexBuilder.js
 date: June 21, 2015
 */
+
+var c = document.querySelector("canvas");
+var ctx = c.getContext("2d"); //ctx stands for context
+var drawMe = { 
+	fill: function(){ctx.fill();},
+	stroke: function(){ctx.fill();},
+}
+
+ctx.fillRect(100,100,100,100);
+ctx.strokeRect(50,50,50,50);
+ctx.strokeRect(75,75,50,50);
+
+$.each(drawMe, function(index, element){
+	ctx.beginPath();
+	ctx.lineTo(50,50);
+	if(index === "fill")
+	{
+		ctx.lineTo(50,10);
+	}
+	else
+	{
+		ctx.lineTo(10,50);
+	}
+	ctx.lineTo(10,10);
+	element;
+	drawMe[index]();
+});
+
