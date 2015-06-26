857fab0b0f6b57a3016bb2cf8c2c24b3137e2a3d
chinesebaloni <berty.ruan@gmail.com>
2015-06-25 20:54:03 -0400

    This sucks....

--- js/blogHtmlDefinitions.js
+++ js/blogHtmlDefinitions.js
@@ -5,7 +5,5 @@ date: June 21, 2015
 info: html declarations to be added to  ../index.html, I wonder if this file is useful at all or not...
 */
 
-var HTMLblogSection =  "<li><h2>%data1%</h2> \
-						<span>Created: %data2%</span> \
-						%data3%</li>";
+var HTMLblogSection =  "<li><h2>%data1%</h2><span>Created: %data2%</span>%data3%</li>";
 
